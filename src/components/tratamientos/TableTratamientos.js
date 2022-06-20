145d4e856341c58fa0248c43412604dff93c853b
Jessy Roblero <jessy.07.2011@gmail.com>
2022-06-19 23:50:07 -0600

    cambié el drop

--- src/components/tratamientos/TableTratamientos.js
+++ src/components/tratamientos/TableTratamientos.js
@@ -6,7 +6,7 @@ export const TableTratamientos = () => {
     <>
       <div className="table-responsive">
         <table className="table table-hover table-bordered " id="tabla">
-          <thead >
+          <thead>
             <tr>
               {/* <th>Item</th> */}
               <th>DESCRIPCION</th>
@@ -19,7 +19,7 @@ export const TableTratamientos = () => {
               <td>Tratamiento gna</td>
               <td>32000</td>
               <td>
-                <div className="btn-group dropup acciones">
+                <div className="btn-group dropstart acciones">
                   <a
                     type="button"
                     data-toggle="dropdown"
@@ -29,21 +29,19 @@ export const TableTratamientos = () => {
                   >
                     {/* <i className="fas fa-ellipsis-v text-warning" ></i> */}
                     <i className="fa-solid fa-bars"></i>
-      
                   </a>
 
-                  <ul className="dropdown-menu z-index-5
-                      ">
+                  <ul
+                    className="dropdown-menu z-index-5
+                      "
+                  >
                     <li>
                       <a
                         // data-toggle="modal"
                         // data-target="#exampleModalEdit{{ $item->id_articulo }}"
                         className="btn btn-md  text-decorated-none dropdown-item  "
                       >
-                        <i
-                          className="fas fa-edit mx-1"
-
-                        ></i>
+                        <i className="fas fa-edit mx-1"></i>
                         Editar
                       </a>
                     </li>
@@ -54,55 +52,163 @@ export const TableTratamientos = () => {
                         // data-target="#exampleModal{{ $item->id_articulo }}"
                         className="btn btn-md  text-decorated-none dropdown-item "
                       >
-                        <i
-                          className="fas fa-eye mx-1"
-                         
-                        ></i>
+                        <i className="fas fa-eye mx-1"></i>
                         Ver
                       </a>
                     </li>
                   </ul>
                 </div>
-                <button type="submit" className="btn btn-md"><i
-                  className="fas fa-trash-alt text-danger"></i></button>
-
+                <button type="submit" className="btn btn-md">
+                  <i className="fas fa-trash-alt text-danger"></i>
+                </button>
               </td>
             </tr>
 
             <tr>
-              <td>Tratamiento ajk</td>
-              <td>4000</td>
-              <td>-</td>
+              <td>Tratamiento gna</td>
+              <td>32000</td>
+              <td>
+                <div className="btn-group dropstart acciones">
+                  <a
+                    type="button"
+                    data-toggle="dropdown"
+                    className="btn btn-md  text-decorated-none "
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    {/* <i className="fas fa-ellipsis-v text-warning" ></i> */}
+                    <i className="fa-solid fa-bars"></i>
+                  </a>
+
+                  <ul
+                    className="dropdown-menu z-index-5
+                      "
+                  >
+                    <li>
+                      <a
+                        // data-toggle="modal"
+                        // data-target="#exampleModalEdit{{ $item->id_articulo }}"
+                        className="btn btn-md  text-decorated-none dropdown-item  "
+                      >
+                        <i className="fas fa-edit mx-1"></i>
+                        Editar
+                      </a>
+                    </li>
+
+                    <li>
+                      <a
+                        // data-toggle="modal"
+                        // data-target="#exampleModal{{ $item->id_articulo }}"
+                        className="btn btn-md  text-decorated-none dropdown-item "
+                      >
+                        <i className="fas fa-eye mx-1"></i>
+                        Ver
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <button type="submit" className="btn btn-md">
+                  <i className="fas fa-trash-alt text-danger"></i>
+                </button>
+              </td>
             </tr>
 
             <tr>
               <td>Tratamiento gna</td>
               <td>32000</td>
-              <td>-</td>
-            </tr>
+              <td>
+                <div className="btn-group dropstart acciones">
+                  <a
+                    type="button"
+                    data-toggle="dropdown"
+                    className="btn btn-md  text-decorated-none "
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    {/* <i className="fas fa-ellipsis-v text-warning" ></i> */}
+                    <i className="fa-solid fa-bars"></i>
+                  </a>
 
-            <tr>
-              <td>Tratamiento ajk</td>
-              <td>4000</td>
-              <td>-</td>
+                  <ul
+                    className="dropdown-menu z-index-5
+                      "
+                  >
+                    <li>
+                      <a
+                        // data-toggle="modal"
+                        // data-target="#exampleModalEdit{{ $item->id_articulo }}"
+                        className="btn btn-md  text-decorated-none dropdown-item  "
+                      >
+                        <i className="fas fa-edit mx-1"></i>
+                        Editar
+                      </a>
+                    </li>
+
+                    <li>
+                      <a
+                        // data-toggle="modal"
+                        // data-target="#exampleModal{{ $item->id_articulo }}"
+                        className="btn btn-md  text-decorated-none dropdown-item "
+                      >
+                        <i className="fas fa-eye mx-1"></i>
+                        Ver
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <button type="submit" className="btn btn-md">
+                  <i className="fas fa-trash-alt text-danger"></i>
+                </button>
+              </td>
             </tr>
 
             <tr>
               <td>Tratamiento gna</td>
               <td>32000</td>
-              <td>-</td>
-            </tr>
+              <td>
+                <div className="btn-group dropstart acciones">
+                  <a
+                    type="button"
+                    data-toggle="dropdown"
+                    className="btn btn-md  text-decorated-none "
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    {/* <i className="fas fa-ellipsis-v text-warning" ></i> */}
+                    <i className="fa-solid fa-bars"></i>
+                  </a>
 
-            <tr>
-              <td>Tratamiento ajk</td>
-              <td>4000</td>
-              <td>-</td>
-            </tr>
+                  <ul
+                    className="dropdown-menu z-index-5
+                      "
+                  >
+                    <li>
+                      <a
+                        // data-toggle="modal"
+                        // data-target="#exampleModalEdit{{ $item->id_articulo }}"
+                        className="btn btn-md  text-decorated-none dropdown-item  "
+                      >
+                        <i className="fas fa-edit mx-1"></i>
+                        Editar
+                      </a>
+                    </li>
 
-            <tr>
-              <td>Tratamiento gna</td>
-              <td>32000</td>
-              <td>-</td>
+                    <li>
+                      <a
+                        // data-toggle="modal"
+                        // data-target="#exampleModal{{ $item->id_articulo }}"
+                        className="btn btn-md  text-decorated-none dropdown-item "
+                      >
+                        <i className="fas fa-eye mx-1"></i>
+                        Ver
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <button type="submit" className="btn btn-md">
+                  <i className="fas fa-trash-alt text-danger"></i>
+                </button>
+              </td>
             </tr>
           </tbody>
         </table>
