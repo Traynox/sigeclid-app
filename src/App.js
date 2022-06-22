216cdfb15c3ac76588ea3b453222d6ab1d309db7
Jessy Roblero <jessy.07.2011@gmail.com>
2022-06-21 19:24:02 -0600

    dash delia

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Content } from "./components-prueba/Content";
+import { Footer } from "./components-prueba/Footer";
+import { Header } from "./components-prueba/Header";
+import { Menu } from "./components-prueba/Menu";
+
 
 function App() {
   return (
     <>
+      <Header />
+      <Menu />
+      <Content />
+      <Footer />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/styles/CSSTheme/bootstrap.css';
 import '../src/styles/CSSTheme/app.css';
-import Sigleclid from './Sigleclid';
+import App from './App';
+// import Sigleclid from './Sigleclid';
 
 
- ReactDOM.render(<Sigleclid />, document.getElementById('root'));
+//  ReactDOM.render(<Sigleclid />, document.getElementById('root'));
+ ReactDOM.render(<App />, document.getElementById('root'));
 
 
 
