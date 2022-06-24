29058a1cacf231841b75b798d4d2e759285a686c
Jessy Roblero <jessy.07.2011@gmail.com>
2022-06-24 17:28:58 -0600

    calendar
    sin redux

--- src/components-prueba/Menu.js
+++ src/components-prueba/Menu.js
@@ -12,25 +12,12 @@ export const Menu = () => {
           className="brand-image img-circle elevation-3"
           // style={{ opacity: ".8" }}
         />
-        <span className="brand-text font-weight-light">AdminLTE 3</span>
+        <span className="brand-text bg-dark">SIGECLID</span>
       </a>
       {/* Sidebar */}
       <div className="sidebar">
         {/* Sidebar user panel (optional) */}
-        <div className="user-panel mt-3 pb-3 mb-3 d-flex">
-          <div className="image">
-            <img
-              src="dist/img/user2-160x160.jpg"
-              className="img-circle elevation-2"
-              alt="User Image"
-            />
-          </div>
-          <div className="info">
-            <a href="#" className="d-block">
-              Alexander Pierce
-            </a>
-          </div>
-        </div>
+     
 
         {/* Sidebar Menu */}
         <nav className="mt-2">
---
+++ src/components/citas/CitasScreen.js
@@ -0,0 +1,60 @@
+import moment from "moment";
+import React, { useState } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import { messages } from "../../helpers/calendar-messages-es";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "moment/locale/es";
+import { CalendarEvent } from "./CalendarEvent";
+
+moment.locale("es");
+const localizer = momentLocalizer(moment); // or globalizeLocalizer
+
+export const CitasScreen = () => {
+
+    const [lastView, setLastView] = useState(
+        localStorage.getItem("lastView") || "month"
+      );
+
+      const onViewChange = (e) => {
+        setLastView(e);
+        localStorage.setItem("lastView", e);
+      };
+
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        const style = {
+          backgroundColor: "#867cf7",
+          borderRadius: "0px",
+          opacity: 0.8,
+          display: "block",
+          color: "white",
+        };
+        return {
+          style,
+        };
+      };
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="card-body">
+          <Calendar
+            localizer={localizer}
+            // events={events}
+            startAccessor="start"
+            endAccessor="end"
+            messages={messages}
+            eventPropGetter={eventStyleGetter}
+            // onDoubleClickEvent={onDoubleClick}
+            // onSelectEvent={onSelectEvent}
+            onView={onViewChange}
+            // onSelectSlot={onSelectSlot}
+            selectable={true}
+            view={lastView}
+            components={{
+              event: CalendarEvent,
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
