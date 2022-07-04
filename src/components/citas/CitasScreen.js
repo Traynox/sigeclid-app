327475fcd3ba78afa8f393aca2a223ede64f1273
Traynox <andex619@gmail.com>
2022-07-04 00:32:00 -0600

    comit

--- src/components/citas/CitasScreen.js
+++ src/components/citas/CitasScreen.js
@@ -11,50 +11,56 @@ const localizer = momentLocalizer(moment); // or globalizeLocalizer
 
 export const CitasScreen = () => {
 
-    const [lastView, setLastView] = useState(
-        localStorage.getItem("lastView") || "month"
-      );
+  const [lastView, setLastView] = useState(
+    localStorage.getItem("lastView") || "month"
+  );
 
-      const onViewChange = (e) => {
-        setLastView(e);
-        localStorage.setItem("lastView", e);
-      };
+  const onViewChange = (e) => {
+    setLastView(e);
+    localStorage.setItem("lastView", e);
+  };
 
-    const eventStyleGetter = (event, start, end, isSelected) => {
-        const style = {
-          backgroundColor: "#867cf7",
-          borderRadius: "0px",
-          opacity: 0.8,
-          display: "block",
-          color: "white",
-        };
-        return {
-          style,
-        };
-      };
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: "#867cf7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      display: "block",
+      color: "white",
+    };
+    return {
+      style,
+    };
+  };
   return (
-    <div className="container">
-      <div className="card">
-        <div className="card-body">
-          <Calendar
-            localizer={localizer}
-            // events={events}
-            startAccessor="start"
-            endAccessor="end"
-            messages={messages}
-            eventPropGetter={eventStyleGetter}
-            // onDoubleClickEvent={onDoubleClick}
-            // onSelectEvent={onSelectEvent}
-            onView={onViewChange}
-            // onSelectSlot={onSelectSlot}
-            selectable={true}
-            view={lastView}
-            components={{
-              event: CalendarEvent,
-            }}
-          />
+    <>
+      <div className="d-flex justify-content-between ">
+        <h1 className='m-4'>Citas</h1>
+
+      </div>
+      <div className="container">
+        <div className="card">
+          <div className="card-body">
+            <Calendar
+              localizer={localizer}
+              // events={events}
+              startAccessor="start"
+              endAccessor="end"
+              messages={messages}
+              eventPropGetter={eventStyleGetter}
+              // onDoubleClickEvent={onDoubleClick}
+              // onSelectEvent={onSelectEvent}
+              onView={onViewChange}
+              // onSelectSlot={onSelectSlot}
+              selectable={true}
+              view={lastView}
+              components={{
+                event: CalendarEvent,
+              }}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
